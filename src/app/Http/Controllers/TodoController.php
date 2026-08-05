5aa77323d14f17fcfab3160aea740d1364dcc5fc
yuma-yasuda0216 <nobiemon021611@gmail.com>
2026-08-05 20:54:17 +0900

    feat:DBから取得したデータをコントローラーに渡す処理を追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -13,6 +13,6 @@ class TodoController extends Controller
         // 追加
         $todo = new Todo();
         $todos = $todo->all();
-        return view('todo.index');
+        return view('todo.index', ['todos' => $todos]);
     }
 }
