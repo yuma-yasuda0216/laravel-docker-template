f891aa7916503f25ffe8d1df7a0aa4c77593fdb3
yuma-yasuda0216 <nobiemon021611@gmail.com>
2026-08-18 20:30:12 +0900

    feat:新規作成画面_フォームから送信した時の処理の作成

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -21,4 +21,18 @@ class TodoController extends Controller
         // TODO: 第1引数を指定
         return view('todo.create'); // 追記
     }
+
+    public function store(Request $request)
+    {
+        $content = $request->input('content'); // 追記
+        
+        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+        $todo = new Todo();
+        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+        $todo->content = $content;
+        // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+        $todo->save();
+
+        return redirect()->route('todo.index'); // 追記
+    }
 }
